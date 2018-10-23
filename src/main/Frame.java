262b3777d3fd91314b46e57d913b4a52e1fb8efe
Jorge <jerez_1998@hotmail.com>
2018-10-23 15:10:42 +0200

    23/10 v2

--- src/main/Frame.java
+++ src/main/Frame.java
@@ -18,8 +18,8 @@ import javax.swing.*;
 public class Frame extends JFrame implements ActionListener{
     private JPanel jpmain;
     private GridLayout gly;
-    private String[] images = { "/images/espadas.png", "/images/bastos.png", "/images/oros.png", "/images/copas.png", "/images/dorso.png" };
-    
+    private String[] images = { "/images/espadas.png", "/images/bastos.png", "/images/oros.png", "/images/copas.png"};
+    private Icon icono = new ImageIcon(getClass().getResource("/images/dorso.png"));
     private JPanel jpjuego;
     private JButton[] cartas;
     private JTextField credito;
@@ -33,19 +33,24 @@ public class Frame extends JFrame implements ActionListener{
         setLocationRelativeTo(null);    //Con aparece en el centro de la pantalla
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         
-        jpmain = new JPanel();
-        add(jpmain);
+        //Panel donde irás las cartas
+        jpmain = new JPanel(); 
+        add(jpmain); //Lo añado a la ventana
         gly = new GridLayout(2, 4, 20, 20);
         
-        cartas = new JButton[6];
-        Icon icono = new ImageIcon(getClass().getResource("/images/dorso.png"));
+        //Botones de las cartass 
+        cartas = new JButton[6]; 
+        //Les pongo dorso
         for (int i = 0; i < 6; i++) {
             cartas[i] = new JButton();
             cartas[i].setIcon(icono);;
+            cartas[i].addActionListener(this);
         }
+        //Panel lateral
         jpjuego = new JPanel();
         
-        
+        //Establezco el layout de el panel principal, meto los botones y el panel lateral
+        jpmain.setLayout(gly);
         jpmain.add(cartas[0]);
         jpmain.add(cartas[1]);
         jpmain.add(cartas[2]);
@@ -53,12 +58,22 @@ public class Frame extends JFrame implements ActionListener{
         jpmain.add(cartas[3]);
         jpmain.add(cartas[4]);
         jpmain.add(cartas[5]);
-        jpmain.setLayout(gly);
     }
     
     @Override
     public void actionPerformed(ActionEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //Genero número aleatorio para seleccionar imagen
+        int num = (int) (Math.random()*4);
+        Icon icono2 = new ImageIcon(getClass().getResource(images[num]));
+        
+        //metodo que aplica una imagen a el boton pulsado
+        for (int i = 0; i < 6; i++) {
+            if (cartas[i].equals(e.getSource()) && cartas[i].getIcon().equals(icono)) { //Solo si tiene el dorso, le pone un palo
+                cartas[i].setIcon(icono2);
+            }
+           
+        }
+        
     }
     
 }
